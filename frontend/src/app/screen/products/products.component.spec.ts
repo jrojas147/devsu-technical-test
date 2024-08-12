5125359cddb9e0f32a8648f3e0536ec27fbbedad
Joan Andres Rojas Ramirez <jarojasramirez147@gmail.com>
2024-08-12 11:20:46 -0500

    ajuste pruebas unitarias

--- frontend/src/app/screen/products/products.component.spec.ts
+++ frontend/src/app/screen/products/products.component.spec.ts
@@ -1,23 +1,92 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductsComponent } from './products.component';
+import { ProductService } from 'src/app/services/product-services/product.service';
+import { Router } from '@angular/router';
+import { ToastService } from 'src/app/components/toast/toast.service';
+import { AccountsListModel, AccountsModel } from 'src/app/models/accounts.model';
+import { of } from 'rxjs';
 
 describe('ProductsComponent', () => {
   let component: ProductsComponent;
   let fixture: ComponentFixture<ProductsComponent>;
+  let productService: jasmine.SpyObj<ProductService>;
+  let router: jasmine.SpyObj<Router>;
+  let toastService: jasmine.SpyObj<ToastService>;
+  let mockIncidencesService: jasmine.SpyObj<ProductService>
 
   beforeEach(async () => {
+    const bankServiceSpy = jasmine.createSpyObj('BankService', [
+      'getAccounts',
+      'deleteAccounts',
+    ]);
+    const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
+    const toastServiceSpy = jasmine.createSpyObj('ToastService', ['show']);
+
     await TestBed.configureTestingModule({
-      declarations: [ ProductsComponent ]
-    })
-    .compileComponents();
+      declarations: [ProductsComponent],
+      providers: [
+        { provide: productService, useValue: bankServiceSpy },
+        { provide: Router, useValue: routerSpy },
+        { provide: ToastService, useValue: toastServiceSpy },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ProductsComponent);
     component = fixture.componentInstance;
+    productService = TestBed.inject(ProductService) as jasmine.SpyObj<ProductService>;
+    router = TestBed.inject(Router) as jasmine.SpyObj<Router>;
+    toastService = TestBed.inject(ToastService) as jasmine.SpyObj<ToastService>;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load list on ngOnInit', () => {
+    const mockAccounts: AccountsListModel = {
+      data: [
+        {
+          id: '1',
+          name: 'Test',
+          logo: '1',
+          description: 'Test description',
+          date_release: new Date('2024-08-01'),
+          date_revision: new Date('2024-08-01'),
+        },
+      ],
+    };
+    productService.getAccounts.and.returnValue(of(mockAccounts));
+
+    component.ngOnInit();
+
+    expect(productService.getAccounts).toHaveBeenCalled();
+    expect(component.listAccounts).toEqual(mockAccounts.data);
+  });
+
+  it('should handle emitTable actions', () => {
+    const mockAccount: AccountsModel = {
+      id: '1',
+          name: 'Test',
+          logo: '1',
+          description: 'Test description',
+          date_release: new Date('2024-08-01'),
+          date_revision: new Date('2024-08-01'),
+    };
+
+    component.emitTable({ action: 'add' });
+    expect(router.navigateByUrl).toHaveBeenCalledWith('/registry');
+
+    component.emitTable({ action: 'edit', data: mockAccount });
+    expect(router.navigateByUrl).toHaveBeenCalledWith('/registry/1');
+
+    const mockResponse: AccountsListModel = { data: [] };
+    productService.deleteAccounts.and.returnValue(of(mockResponse));
+    component.emitTable({ action: 'delete', data: mockAccount });
+    expect(productService.deleteAccounts).toHaveBeenCalledWith(mockAccount);
+    expect(toastService.show).toHaveBeenCalledWith('Exitoso', '');
+    expect(productService.getAccounts).toHaveBeenCalled();
   });
 });
--- frontend/src/app/screen/register-product/register-product.component.spec.ts
+++ frontend/src/app/screen/register-product/register-product.component.spec.ts
@@ -1,23 +1,158 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { RegisterProductComponent } from './register-product.component';
+import { ProductService } from 'src/app/services/product-services/product.service';
+import { ToastService } from 'src/app/components/toast/toast.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RegisterProductModule } from './register-product.module';
+import { RouterTestingModule } from '@angular/router/testing';
+import * as moment from 'moment';
+import { of } from 'rxjs';
+
 
 describe('RegisterProductComponent', () => {
+
   let component: RegisterProductComponent;
   let fixture: ComponentFixture<RegisterProductComponent>;
+  let productService: jasmine.SpyObj<ProductService>;
+  let toastService: jasmine.SpyObj<ToastService>;
+  let router: jasmine.SpyObj<Router>;
+  let activatedRoute: jasmine.SpyObj<ActivatedRoute>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ RegisterProductComponent ]
-    })
-    .compileComponents();
+    const bankServiceSpy = jasmine.createSpyObj('BankService', [
+      'getAccountsById',
+      'putAccounts',
+      'postAccounts',
+      'verifyAccount',
+    ]);
+    const toastServiceSpy = jasmine.createSpyObj('ToastService', ['show']);
+    const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
+    const activatedRouteSpy = jasmine.createSpyObj('ActivatedRoute', ['snapshot'], {
+      snapshot: {
+        paramMap: {
+          get: jasmine.createSpy().and.returnValue(null)  // Mock `paramMap.get` to return null
+        }
+      }
+    });
 
+    await TestBed.configureTestingModule({
+      declarations: [
+        RegisterProductComponent
+      ],
+      imports: [
+        RouterTestingModule, 
+        RegisterProductModule],
+      providers: [
+        { provide: ProductService, useValue: bankServiceSpy },
+        { provide: Router, useValue: routerSpy },
+        { provide: ActivatedRoute, useValue: activatedRouteSpy }
+      ],
+    }).compileComponents();
+
+    toastService = TestBed.inject(ToastService) as jasmine.SpyObj<ToastService>;
+    router = TestBed.inject(Router) as jasmine.SpyObj<Router>;
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(RegisterProductComponent);
     component = fixture.componentInstance;
+    productService = TestBed.inject(ProductService) as jasmine.SpyObj<ProductService>;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize form with controls', () => {
+    component.ngOnInit();
+
+    expect(component.formAccounts.contains('id')).toBeTrue();
+    expect(component.formAccounts.contains('name')).toBeTrue();
+    expect(component.formAccounts.contains('description')).toBeTrue();
+    expect(component.formAccounts.contains('logo')).toBeTrue();
+    expect(component.formAccounts.contains('date_release')).toBeTrue();
+  });
+
+  it('should disable id control if idAccount exists', () => {
+    component.idAccount = '123';
+    component.ngOnInit();
+    expect(component.formAccounts.get('id')?.disabled).toBeTrue();
+  });
+
+  it('should call checkIdAccount on id value change', () => {
+    const checkIdAccountSpy = spyOn(component, 'checkIdAccount');
+    component.ngOnInit();
+    const idControl = component.formAccounts.get('id');
+    idControl?.setValue('123');
+    expect(checkIdAccountSpy).toHaveBeenCalledWith('123');
+  });
+
+  it('should set control values with account data', () => {
+    const mockAccount = {
+      id: '1',
+      name: 'Test',
+      logo: '1',
+      description: 'Test description',
+      date_release: new Date('2024-08-01'),
+      date_revision: moment().format('YYYY-MM-DD'),
+    };
+    productService.getAccountsById.and.returnValue(of(mockAccount));
+    component.idAccount = '1';
+    component.ngOnInit();
+    fixture.detectChanges();
+    expect(component.formAccounts.get('name')?.value).toBe('Test');
+    expect(component.formAccounts.get('description')?.value).toBe(
+      'Test description'
+    );
+  });
+
+  it('should set control values with account data', () => {
+    const mockAccount = {
+      id: '1',
+      name: 'Test',
+      logo: '1',
+      description: 'Test description',
+      date_release: new Date('2024-08-01'),
+      date_revision: moment().format('YYYY-MM-DD'),
+    };
+    productService.getAccountsById.and.returnValue(of(mockAccount));
+    component.idAccount = '1';
+    component.ngOnInit();
+    fixture.detectChanges();
+    expect(component.formAccounts.get('name')?.value).toBe('Test');
+    expect(component.formAccounts.get('description')?.value).toBe('Test description');
+  });
+
+  it('should call saveRegistry and navigate on success', () => {
+    const mockResponse = { data: [], message: 'Success' };
+    productService.postAccounts.and.returnValue(of(mockResponse));
+    component.saveRegistry();
+    expect(productService.postAccounts).toHaveBeenCalled();
+    expect(toastService.show).toMatch;
+    expect(router.navigateByUrl).toMatch;
+  });
+
+  it('should handle update and call putAccounts', () => {
+    const mockResponse = { data: [], message: 'Updated' };
+    component.idAccount = '1';
+    productService.putAccounts.and.returnValue(of(mockResponse));
+    component.saveRegistry();
+    expect(productService.putAccounts).toHaveBeenCalled();
+    expect(toastService.show).toMatch
+    expect(router.navigateByUrl).toMatch;
+  });
+
+  it('should call backUrl', () => {
+
+    component.backUrl();
+    expect(router.navigateByUrl).toMatch;
+  });
+
+  it('should check if account exists and set existAccount', () => {
+    productService.verifyAccount.and.returnValue(of(true));
+    component.checkIdAccount('123');
+    expect(productService.verifyAccount).toHaveBeenCalledWith('123');
+    expect(component.existAccount).toBeTrue();
   });
 });
