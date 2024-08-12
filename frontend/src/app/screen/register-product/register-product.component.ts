4337248e4e79e0ec0f5a65e1835ede7aa8e8b0f7
Joan Andres Rojas Ramirez <jarojasramirez147@gmail.com>
2024-08-12 10:53:14 -0500

    documentacion componente register product

--- frontend/src/app/screen/register-product/register-product.component.ts
+++ frontend/src/app/screen/register-product/register-product.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import * as moment from 'moment';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ModalService } from 'src/app/services/modal-services/modal.service';
 import { AccountsListModel, AccountsModel } from 'src/app/models/accounts.model';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ProductService } from 'src/app/services/product-services/product.service';
 import { ToastService } from 'src/app/components/toast/toast.service';
 
+/**
+ * Register Product
+ * 
+ */
 @Component({
   selector: 'app-register-product',
   templateUrl: './register-product.component.html',
@@ -31,32 +34,28 @@ export class RegisterProductComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    console.log('Entra');
     this.formAccounts = this.fb.group(this.createControls());
-
     this.formAccounts.controls['id'].valueChanges.subscribe(
       (selectedValue: string) => {
-        console.log(selectedValue);
         this.checkIdAccount(selectedValue);
       }
     );
-
     if (this.idAccount) {
       this.productService
         .getAccountsById(this.idAccount)
         ?.subscribe((response: any) => {
           this.account = response;
-
           this.setControlValues(this.account);
         });
     }
-
-    console.log(this.formAccounts);
   }
 
+  /**
+   * Permite la creacion de controles de formularo definiendo validaicones teniedas en cuenta por el Dto ofrecido para simular el servicio
+   * @returns Formulario 
+   */
   public createControls() {
     const groupControl: any = {};
-
     groupControl['id'] = [
       {
         value: null,
@@ -75,30 +74,42 @@ export class RegisterProductComponent implements OnInit {
       },
       Validators.required,
     ];
-
     return groupControl;
   }
 
+  /**
+   * Permite cargar valores de formulario
+   * @param account datos a cargar
+   */
   setControlValues(account: any) {
     Object.keys(this.formAccounts?.controls).forEach((key) => {
       this.formAccounts?.controls[key].setValue(account[key]);
     });
   }
 
-  checkIdAccount(idAccount: string) {
+  /**
+   * Permite realizar validacion si existe producto mediante el metodo verification
+   * y asi definir si habilita o deshabilita visualmente asi como mostrar mensaje de error
+   * para evitar ingresar id duplicados
+   * @param idAccount id de producto a consultar
+   */
+  public checkIdAccount(idAccount: string) {
     this.productService.verifyAccount(idAccount).subscribe((response: boolean) => {
       this.existAccount = response;
     });
   }
 
-  saveRegistry() {
+  /**
+   * Permite guardar la informacion de producto, desde la carga de componente se identifica tenemos un param id
+   * de ser asi se considera como una edicion, de caso contrario seria adicionar nuevo producto
+   * para cada caso de tiene un itervalo de tiempo para visualizar confirmacion de consumo de servicio
+   */
+  public saveRegistry(): void {
     const dataForm = this.formAccounts.getRawValue();
-
     if (this.idAccount) {
       this.productService.putAccounts(dataForm)?.subscribe((response: AccountsListModel) => {
         this.listAccounts = response.data;
         this.toastService.show('Actualizado', response.message || '');
-
         setTimeout(() => {
           this.backUrl();
         }, 1000);
@@ -107,9 +118,7 @@ export class RegisterProductComponent implements OnInit {
     } else {
       this.productService.postAccounts(dataForm)?.subscribe((response: AccountsListModel) => {
         this.listAccounts = response.data;
-
         this.toastService.show('Exitoso', response.message || '');
-
         setTimeout(() => {
           this.backUrl();
         }, 1000);
@@ -117,9 +126,10 @@ export class RegisterProductComponent implements OnInit {
     }
   }
 
-  backUrl() {
+  /**
+   * Permite retornar url prinicipal, pantalla de productos activos
+   */
+  public backUrl(): void {
     this.router.navigateByUrl('/');
   }
-
-
 }
